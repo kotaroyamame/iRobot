225899422e5f9c537d6846a1e92437fe7b6faa6d
kotaroyamame <yamame@fi.chu.jp>
2017-05-21 00:25:51 +0900

    主な処理を関数にまとめ、補正係数、スピードなどの設定値を行頭に定数としてまとめた

--- Kobutan/Bin/Commanders/RoombaSample1/Script/RoombaMain.cs
+++ Kobutan/Bin/Commanders/RoombaSample1/Script/RoombaMain.cs
@@ -1,7 +1,74 @@
 ﻿
 
 
+//設定値
+ const int X = 1000; // 001
+ const int Y = 2000; // 010
+//スピード
+ const int SPEED = 30;
+//旋回時のスピード
+const int TURN_SPEED = 30;
 
+//直進時の補正　-で右補正、＋で左補正
+const int FORWARD_CORRECTION_FACTOR = 0;
+//距離の補正　-で右補正、＋で左補正
+const int DISTANCE_CORRECTION_FACTOR = 0;
+
+void MaryHadALittleLamb(){
+    int oneTempo=700;
+    int oneEighth=(int)oneTempo/2;
+    Sound_MI();
+    Sleep(oneEighth*3);
+    Sound_RE();
+    Sleep(oneEighth*1);
+    Sound_DO();
+    Sleep(oneEighth*2);
+    Sound_RE();
+    Sleep(oneEighth*2);
+    Sound_MI();
+    Sleep(oneEighth*2);
+    Sound_MI();
+    Sleep(oneEighth*2);
+    Sound_MI();
+    Sleep(oneEighth*4);
+    Sound_RE();
+    Sleep(oneEighth*2);
+    Sound_RE();
+    Sleep(oneEighth*2);
+    Sound_RE();
+    Sleep(oneEighth*4);
+    Sound_MI();
+    Sleep(oneEighth*2);
+    Sound_SO();
+    Sleep(oneEighth*2);
+    Sound_SO();
+    Sleep(oneEighth*4);
+    Sound_MI();
+    Sleep(oneEighth*3);
+    Sound_RE();
+    Sleep(oneEighth*1);
+    Sound_DO();
+    Sleep(oneEighth*2);
+    Sound_RE();
+    Sleep(oneEighth*2);
+    Sound_MI();
+    Sleep(oneEighth*2);
+    Sound_MI();
+    Sleep(oneEighth*2);
+    Sound_MI();
+    Sleep(oneEighth*4);
+    Sound_RE();
+    Sleep(oneEighth*2);
+    Sound_RE();
+    Sleep(oneEighth*2);
+    Sound_MI();
+    Sleep(oneEighth*4);
+    Sound_RE();
+    Sleep(oneEighth*2);
+    Sound_DO();
+    Sleep(oneEighth*8);
+
+}
 class ImpactCalculator{
     private double distance=0;
     private int x;
@@ -56,45 +123,103 @@ class ImpactCalculator{
     }
 }
 
+void DoDrive(int _distance){
+    //距離に補正係数をかける
+    int distance = _distance + DISTANCE_CORRECTION_FACTOR;
+    
+    Encoder_Initialize(ref _LeftEnc, LeftEncoder);
+    Encoder_Initialize(ref _RightEnc, RightEncoder);
+    Encoder_Update(ref _LeftEnc, LeftEncoder);
+    Encoder_Update(ref _RightEnc, RightEncoder);
+    UpdateDistanceAngle();
+    //P14
+    OutputString("distance: " + _Distance);
+    BaseControl(100, 0);
+    PWMControl(100+(FORWARD_CORRECTION_FACTOR), 100-(FORWARD_CORRECTION_FACTOR));
+    while (_Distance < distance)
+    {
+        Encoder_Update(ref _LeftEnc, LeftEncoder);
+        Encoder_Update(ref _RightEnc, RightEncoder);
+        UpdateDistanceAngle();
+        OutputString("" + _LeftEnc.Value + " " + _RightEnc.Value);
+        OutputString("distance: " + _Distance);
+        Sleep(1);
+    }
+}
+
+void DoturnRight(){
+    Encoder_Initialize(ref _LeftEnc, LeftEncoder);
+    Encoder_Initialize(ref _RightEnc, RightEncoder);
+    Encoder_Update(ref _LeftEnc, LeftEncoder);
+    Encoder_Update(ref _RightEnc, RightEncoder);
+    UpdateDistanceAngle();
+    //P14
+    OutputString("distance: " + _Distance + "angle:" + _Angle);
+    BaseControl(100, 10);
+    while (_Angle < 90)
+    {
+        Encoder_Update(ref _LeftEnc, LeftEncoder);
+        Encoder_Update(ref _RightEnc, RightEncoder);
+        UpdateDistanceAngle();
+        OutputString("" + _LeftEnc.Value + " " + _RightEnc.Value);
+        OutputString("distance: " + _Distance);
+        Sleep(1);
+    }
+}
+
+void Doturn(int angle){
+    Encoder_Initialize(ref _LeftEnc, LeftEncoder);
+    Encoder_Initialize(ref _RightEnc, RightEncoder);
+    Encoder_Update(ref _LeftEnc, LeftEncoder);
+    Encoder_Update(ref _RightEnc, RightEncoder);
+    UpdateDistanceAngle();
+    //P14
+    OutputString("distance: " + _Distance + "angle:" + _Angle);
+    //右ターンなら
+    if(angle<180)
+    {
+        BaseControl(100, -1);
+        while (_Angle < angle)
+        {
+            Encoder_Update(ref _LeftEnc, LeftEncoder);
+            Encoder_Update(ref _RightEnc, RightEncoder);
+            UpdateDistanceAngle();
+            OutputString("" + _LeftEnc.Value + " " + _RightEnc.Value);
+            OutputString("_Angle: " + _Angle);
+            Sleep(1);
+        }
+    }
+    //左ターンなら
+    else
+    {
+        BaseControl(100, 1);
+        while (Math.Abs(_Angle) < (360-angle))
+        {
+            Encoder_Update(ref _LeftEnc, LeftEncoder);
+            Encoder_Update(ref _RightEnc, RightEncoder);
+            UpdateDistanceAngle();
+            OutputString("" + _LeftEnc.Value + " " + _RightEnc.Value);
+            OutputString("_Angle: " + _Angle);
+            Sleep(1);
+        }
+    }
+    
+    
+}
+
+void XYDrive(int distance,int angle){
+    Doturn(angle);
+    DoDrive(distance);
+}
 
 // エントリポイント
 void RoombaMain()
 {
-    ImpactCalculator calculator1=new ImpactCalculator(1000,500);
-
+    ImpactCalculator calculator1=new ImpactCalculator(X,Y);
     OutputString("変えるべき角度は"+calculator1.getAngle()+"度で、進むべき距離は"+calculator1.getDistance()+"です");
-    // int angle=0;
-    // while(angle<=hoge.getAngle()){
-    //     anglee+=Angle;
-    //     OutputInteger(Angle);
-    //     Sleep(1);
-    // }
-    // // BaseControl(100,0);
-    // int distance=0;
-    // while (distance<hoge.getDistance()){
-    //     distance+=Distance;
-    //     OutputInteger(Distance);
-    //     Sleep(1);
-    // }
-        Stop();
+    XYDrive((int)calculator1.getDistance(),(int)calculator1.getAngle());
+    // MaryHadALittleLamb();
+    // DoDrive(1000);
+    Stop();
     Exit();
-    // Encoder_Initialize(ref _LeftEnc, LeftEncoder);
-    // Encoder_Initialize(ref _RightEnc, RightEncoder);
-    // Encoder_Update(ref _LeftEnc, LeftEncoder);
-    // Encoder_Update(ref _RightEnc, RightEncoder);
-    // UpdateDistanceAngle();
-    // //P14
-    // OutputString("distance: " + _Distance);
-    // BaseControl(100, 0);
-    // while (_Distance < hoge.getDistance())
-    // {
-    //     Encoder_Update(ref _LeftEnc, LeftEncoder);
-    //     Encoder_Update(ref _RightEnc, RightEncoder);
-    //     UpdateDistanceAngle();
-    //     OutputString("" + _LeftEnc.Value + " " + _RightEnc.Value);
-    //     OutputString("distance: " + _Distance);
-    //     Sleep(1);
-    // }
-    // Stop();
-    // Exit();
 }
