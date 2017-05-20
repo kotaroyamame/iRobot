0f5239af7c9b99ca59d1241cda673eb172c47e76
kotaroyamame <yamame@fi.chu.jp>
2017-05-20 16:24:28 +0900

    ###

--- Kobutan/Bin/Commanders/RoombaSample1/Script/RoombaMain.cs
+++ Kobutan/Bin/Commanders/RoombaSample1/Script/RoombaMain.cs
@@ -15,7 +15,7 @@ class Hoge{
     private void init(){
         this.distance=Math.Sqrt(System.Math.Pow(this.x,2)+System.Math.Pow(this.y,2));
         double angle=Math.Asin((double)this.y/this.distance);
-        this.angle=angle*(180/Math.PI);
+        this.angle=(int)(angle*(180/Math.PI));
     }
     public double getDistance(){
         return this.distance;
@@ -26,51 +26,48 @@ class Hoge{
         //keisan
     }
 }
-}
 
 
 // エントリポイント
 void RoombaMain()
 {
 
-    int xy[2]={-50,50};
-
     // int distance=0;
     // distance = xy[0]*xy[0]+xy[1]*xy[1];
-    Hoge hoge=new Hoge(xy[0],xy[1]);
+    Hoge hoge=new Hoge(1000,1000);
 
     OutputString("角度は"+hoge.getDistance());
-    int angle=0;
-    while(angle<=hoge.getAngle()){
-        anglee+=Angle;
-        OutputInteger(Angle);
+    // int angle=0;
+    // while(angle<=hoge.getAngle()){
+    //     anglee+=Angle;
+    //     OutputInteger(Angle);
+    //     Sleep(1);
+    // }
+    // // BaseControl(100,0);
+    // int distance=0;
+    // while (distance<hoge.getDistance()){
+    //     distance+=Distance;
+    //     OutputInteger(Distance);
+    //     Sleep(1);
+    // }
+    
+    Encoder_Initialize(ref _LeftEnc, LeftEncoder);
+    Encoder_Initialize(ref _RightEnc, RightEncoder);
+    Encoder_Update(ref _LeftEnc, LeftEncoder);
+    Encoder_Update(ref _RightEnc, RightEncoder);
+    UpdateDistanceAngle();
+    //P14
+    OutputString("distance: " + _Distance);
+    BaseControl(100, 0);
+    while (_Distance < hoge.getDistance())
+    {
+        Encoder_Update(ref _LeftEnc, LeftEncoder);
+        Encoder_Update(ref _RightEnc, RightEncoder);
+        UpdateDistanceAngle();
+        OutputString("" + _LeftEnc.Value + " " + _RightEnc.Value);
+        OutputString("distance: " + _Distance);
         Sleep(1);
     }
-    // BaseControl(100,0);
-    int distance=0;
-    while (distance<hoge.getDistance()){
-        distance+=Distance;
-        OutputInteger(Distance);
-        Sleep(1);
-    }
-    
-    
-    
-    // OutputString("START!");
-    // SetSpeed(8);
-    // OutputString("setSpeed");
-    // // 1秒間 前進
-    // Forward();        // 前進
-    Sleep(3000);      // 次のステップに移るまで1秒待つ
-    Stop();           // ストップ
-    // // 2.5秒間 左旋回
-    // TurnRight();       // 左旋回
-    // Sleep(5500);      // 次のステップに移るまで2.5秒待つ
-    // Stop();           // ストップ
-    // // 1秒間 前進
-    // Forward();        // 前進
-    // Sleep(1000);      // 次のステップに移るまで1秒待つ
-    // Stop();           // ストップ
-    // OutputString("STOP!!");
+    Stop();
     Exit();
 }
